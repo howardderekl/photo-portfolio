92b32358e81e5f3dc9ff20edab7747f2026a88e0
Howard <dhh3742@C00.CORP.COM>
2017-05-11 14:47:29 -0700

    cleaned up an error with the app-routing import from the previous clean-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from "app/home/home.component";
 
 const routes: Routes = [
-  {path: '', children: [] },
-  {path: 'home', component: HomeComponent}
+  {path: '', children: [] }
 ];
 
 @NgModule({
