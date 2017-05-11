8c0fa88d620373e0c11ab37a3c0fc60eae8c9782
Howard <dhh3742@C00.CORP.COM>
2017-05-10 17:01:13 -0700

    Initial Canvas Theme imported

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "app/home/home.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'photo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Bar Open A Photo Gallary';
 }
